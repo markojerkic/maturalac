92afc753293450acd08e230f819a4b4520cdea8d
Marko Jekrić <markojerkic266@gmail.com>
2022-10-08 16:10:48 +0200

    ssr

--- maturalac-next-app/src/utils/trpc.ts
+++ maturalac-next-app/src/utils/trpc.ts
@@ -11,7 +11,7 @@ const getBaseUrl = () => {
 };
 
 export const trpc = createTRPCNext<AppRouter>({
-  config() {
+  config({ ctx }) {
     return {
       transformer: superjson,
       links: [
@@ -22,9 +22,27 @@ export const trpc = createTRPCNext<AppRouter>({
         }),
         httpBatchLink({
           url: `${getBaseUrl()}/api/trpc`,
+          headers() {
+            if (ctx?.req) {
+              // To use SSR properly, you need to forward the client's headers to the server
+              // This is so you can pass through things like cookies when we're server-side rendering
+              // If you're using Node 18, omit the "connection" header
+              const {
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                connection: _connection,
+                ...headers
+              } = ctx.req.headers;
+              return {
+                ...headers,
+                // Optional: inform server that it's an SSR request
+                'x-ssr': '1',
+              };
+            }
+            return {}
+          }
         }),
       ],
     };
   },
-  ssr: false,
+  ssr: true,
 });
